4be26bb5ced829074e9ad99a6b4ffac21599eecd
Manasi <manasi@gmail.com>
2026-08-15 10:06:02 +0530

    first commit

---
+++ src/main/java/com/growfinix/secure_user_authentication/SecureUserAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.growfinix.secure_user_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureUserAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureUserAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/growfinix/secure_user_authentication/SecureUserAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growfinix.secure_user_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureUserAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
